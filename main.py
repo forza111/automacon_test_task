931af652e547046332179976ace6be085275b956
forza111 <n94i3k6forza>
2021-10-09 09:48:17 +0300

    add docker files

---
+++ main.py
@@ -0,0 +1,17 @@
+import uvicorn
+from fastapi import FastAPI
+
+from database import engine, Base
+from routers import users,authentication
+
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(authentication.router)
+app.include_router(users.router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
