9fc44d7b7ca6ad93ea699f7495538fb16ff77167
forza111 <nikitos.ionkin@yandex.ru>
2022-05-30 00:19:00 +0300

    change docker-compose.yml add healthcheck
    add healthcheck 1

--- dependencies.py
+++ dependencies.py
@@ -6,11 +6,4 @@ SECRET_KEY = "461554dcb6e1169277a0658acdb9b4634caf0c901fa63aaf5d3d9aa48f141056"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 1
 
-
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-
-
-
-
